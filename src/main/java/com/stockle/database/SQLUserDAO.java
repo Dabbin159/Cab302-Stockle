a1b6571b9cd991331f33c2593cedb7ac2bbedfe4
Ethan Bell <ethbot1112@gmail.com>
2026-04-23 08:59:12 +1000

    Finished basic user DAO

--- src/main/java/com/stockle/database/SQLUserDAO.java
+++ src/main/java/com/stockle/database/SQLUserDAO.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.time.LocalDate;
 
 import com.stockle.model.User;
 
@@ -25,7 +26,18 @@ public class SQLUserDAO implements UserDAO {
 
     private static final String ADD_USER = "INSERT INTO users (username, password, email, firstName, lastName, dateOfBirth) VALUES (?, ?, ?, ?, ?, ?)";
 
+    private static final String DELETE_USER = "DELETE FROM users WHERE id = ?";
+
+    private static final String UPDATE_USER = "UPDATE users SET username = ?, password = ?, email = ?, firstName = ?, lastName = ?, dateOfBirth = ? WHERE id = ?";
+
+    private static final String GET_USER_BY_ID = "SELECT * FROM users WHERE id = ?";
+
+    private static final String GET_ALL_USERS = "SELECT * FROM users";
+
     @Override
+    /**
+     * @param User The user to add.
+     */
     public void addUser(User user) {
            try {
             PreparedStatement statement = connection.prepareStatement(ADD_USER);
@@ -39,32 +51,96 @@ public class SQLUserDAO implements UserDAO {
             ResultSet generatedKeys = statement.getGeneratedKeys();
             if (generatedKeys.next()) {
                 user.setId(generatedKeys.getInt(1));
-            }
+            } // Sets the generated user ID to the User
         } catch (SQLException ex) {
             ex.printStackTrace();
         }
     }
+    /**
+     * @param User The user to delete.
+     */
     @Override
-    public void deleteUser(int id) {
-        // TODO Auto-generated method stub
-
+    public void deleteUser(User user) {
+        try {
+            PreparedStatement statement = connection.prepareStatement(DELETE_USER);
+            statement.setInt(1, user.getId());
+            statement.executeUpdate();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
     }
 
     @Override
+    /**
+     * @param User The user to update.
+     */
     public void updateUser(User user) {
-        // TODO Auto-generated method stub
-
+        try {
+            PreparedStatement statement = connection.prepareStatement(UPDATE_USER);
+            statement.setString(1, user.getUsername());
+            statement.setString(2, user.getPassword());
+            statement.setString(3, user.getEmail());
+            statement.setString(4, user.getFirstName());
+            statement.setString(5, user.getLastName());
+            statement.setString(6, user.getDateOfBirth().toString());
+            statement.setInt(7, user.getId());
+            statement.executeUpdate();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
     }
 
+    /**
+     * @param id The ID of the user to retrieve.
+     * @return The user with the specified ID, or null if not found.
+     */
     @Override
     public User getUserById(int id) {
-        // TODO Auto-generated method stub
+        try {
+            PreparedStatement statement = connection.prepareStatement(GET_USER_BY_ID);
+            statement.setInt(1, id);
+            ResultSet resultSet = statement.executeQuery();
+            if (resultSet.next()) {
+                String username = resultSet.getString("username");
+                String password = resultSet.getString("password");
+                String email = resultSet.getString("email");
+                String firstName = resultSet.getString("firstName");
+                String lastName = resultSet.getString("lastName");
+                LocalDate dateOfBirth = LocalDate.parse(resultSet.getString("dateOfBirth"));
+                User user = new User(username, password, email, firstName, lastName, dateOfBirth);
+                user.setId(resultSet.getInt("id"));
+                return user;
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
         return null;
     }
 
+    /**
+    * @return A list of all users, or null if no users are found.
+    */
     @Override
     public java.util.List<User> getAllUsers() {
-        // TODO Auto-generated method stub
+        try {
+            PreparedStatement statement = connection.prepareStatement(GET_ALL_USERS);
+            ResultSet resultSet = statement.executeQuery();
+            java.util.List<User> users = new java.util.ArrayList<>();
+            while (resultSet.next()) {
+                String username = resultSet.getString("username");
+                String password = resultSet.getString("password");
+                String email = resultSet.getString("email");
+                String firstName = resultSet.getString("firstName");
+                String lastName = resultSet.getString("lastName");
+                LocalDate dateOfBirth = LocalDate.parse(resultSet.getString("dateOfBirth"));
+                User user = new User(username, password, email, firstName, lastName, dateOfBirth);
+                user.setId(resultSet.getInt("id"));
+                users.add(user);
+            }
+            return users;
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
         return null;
     }
     
--- src/main/java/com/stockle/database/UserDAO.java
+++ src/main/java/com/stockle/database/UserDAO.java
@@ -6,14 +6,35 @@ import com.stockle.model.User;
 
 public interface UserDAO {
 
+    /**
+     * Adds a new user to the database.
+     * @param User The user to add.
+     */
     public void addUser(User user);
 
-    public void deleteUser(int id);
+    /**
+     * Deletes a user from the database.
+     * @param User The user to delete.
+     */
+    public void deleteUser(User user);
 
+    /**
+    * Updates a user's information in the database.
+    * @param User The user to update.
+    */
     public void updateUser(User user);
 
+    /**
+     * Retrieves a user from the database by their ID.
+     * @param id The ID of the user to retrieve.
+     * @return The user with the specified ID, or null if not found.
+     */
     public User getUserById(int id);
 
+    /**
+     * Retrieves all users from the database.
+     * @return A list of all users.
+     */ 
     public List<User> getAllUsers();
 
 
