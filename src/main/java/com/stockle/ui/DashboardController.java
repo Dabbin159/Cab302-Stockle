e1fa29f09f4b576885f9d8187f534873cc8565d7
Jackson Fleming <jacksonlefleming@gmail.com>
2026-04-22 20:32:25 +1000

    dashboard ui

--- src/main/java/com/stockle/ui/DashboardController.java
+++ src/main/java/com/stockle/ui/DashboardController.java
@@ -2,12 +2,8 @@ package com.stockle.ui;
 
 import java.io.IOException;
 
-import javafx.fxml.FXML;
 
 public class DashboardController {
 
-    @FXML
-    private void handleSignOut() throws IOException {
-        SceneManager.switchTo("auth/auth-view.fxml");
-    }
+    
 }
