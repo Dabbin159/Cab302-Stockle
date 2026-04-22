73d344ec4a41e6602527e1e96f7daa6080aac3e1
Ethan Bell <ethbot1112@gmail.com>
2026-04-23 08:50:10 +1000

    Updated test file modularity

--- src/main/java/com/stockle/test/DB_Test.java
+++ src/main/java/com/stockle/test/DB_Test.java
@@ -8,6 +8,10 @@ import com.stockle.model.User;
 public class DB_Test {
 
     public static void main(String[] args) {
+        AddUserTest();
+    }
+
+    public static void AddUserTest() {
         SQLUserDAO userDAO = SQLUserDAO.getInstance();
         User user = new User("TestUsername", "TestPassword", "test@example.com", "Test", "User",LocalDate.now());
         userDAO.addUser(user);
