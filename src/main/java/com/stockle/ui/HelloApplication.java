9fcf91370e4cbdba5c5d655dd49dcb8ec7f02ae5
Jackson Fleming <jacksonlefleming@gmail.com>
2026-04-22 13:24:22 +1000

    fixed alignment

--- src/main/java/com/stockle/ui/HelloApplication.java
+++ src/main/java/com/stockle/ui/HelloApplication.java
@@ -14,8 +14,8 @@ public class HelloApplication extends Application {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("auth/auth-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1100, 700);
         stage.setTitle("Stockle");
-        stage.setMinWidth(800);
-        stage.setMinHeight(600);
+        stage.setMinWidth(1100);
+        stage.setMinHeight(700);
         stage.setScene(scene);
         stage.show();
     }
