2a276d193c0a59e34cd9fd9fa8fbe634a4808c3b
JJTSmasher <jamescannon0514@gmail.com>
2026-04-29 10:42:24 +1000

    fix conflicts

--- src/main/java/com/stockle/model/Trade.java
+++ src/main/java/com/stockle/model/Trade.java
@@ -85,34 +85,6 @@ public class Trade {
         String timeStamp = json.getString("timeStamp");
         return new Trade(id, userNumber, stock, type, quantity, totalValue, timeStamp);
     }
-
-    public int getId() {
-        return id;
-    }
-
-    public int getUserNumber() {
-        return userNumber;
-    }
-
-    public Stock getStock() {
-        return stock;
-    }
-
-    public Boolean isType() {
-        return type;
-    }
-
-    public long getQuantity() {
-        return quantity;
-    }
-
-    public Long getTotalValue() {
-        return totalValue;
-    }
-
-    public String getTimeStamp() {
-        return timeStamp;
-    }
     public void setType(Boolean type) {
         this.type = type;
     }
