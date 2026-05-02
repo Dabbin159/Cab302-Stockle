85b078ed4b1f2d6e826002a95fbfb1895a1a0ddf
Ethan Bell <ethbot1112@gmail.com>
2026-05-02 11:39:59 +1000

    Basic Get, Delete and Insert Methods for Favourites

--- src/main/java/com/stockle/database/SQLFavouritesDAO.java
+++ src/main/java/com/stockle/database/SQLFavouritesDAO.java
@@ -1,7 +1,11 @@
 package com.stockle.database;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class SQLFavouritesDAO implements FavouritesDAO {
@@ -20,19 +24,50 @@ public class SQLFavouritesDAO implements FavouritesDAO {
         return instance;
     }
 
+    private static final String ADD_FAVOURITE = "INSERT INTO favourites (userID, favouritesList) VALUES (?, ?)";
+    private static final String DELETE_FAVOURITE = "DELETE FROM favourites where userID = ?";
+    private static final String GET_FAVOURITES = "SELECT favouritesList FROM favourites where userID = ?";
+
     @Override
-    public Boolean addFavourite(int userID, String favourites) {
-        return false;
+    public void addFavourite(int userID, List<String> favourites) {
+        try {
+            PreparedStatement statement = connection.prepareStatement(ADD_FAVOURITE);
+            statement.setInt(1, userID);
+            statement.setString(2, String.join(",", favourites));
+            statement.executeUpdate();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
     }
 
     @Override
-    public Boolean deleteFavourite(int userID, String favourites){
-        return false;
+    public void deleteFavourite(int userID, String favourites){
+        try {
+            PreparedStatement statement = connection.prepareStatement(DELETE_FAVOURITE);
+            statement.setInt(1, userID);
+            statement.executeUpdate();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
     }
 
     @Override
-    public String getFavourites(int userID){
-        List<String> favouritesList = new ArrayList<>();
+    public List<String> getFavourites(int userID){
+        try {
+            PreparedStatement statement = connection.prepareStatement(GET_FAVOURITES);
+            statement.setInt(1, userID);
+            ResultSet resultSet = statement.executeQuery();
+            List<String> favourites = new ArrayList<>();
+            while (resultSet.next()) {
+                String favouritesString = resultSet.getString("favouritesList");
+                if (favouritesString != null){
+                    favourites.addAll(Arrays.asList(favouritesString.split(",")));
+                }
+            }
+            return favourites;
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
         return null;
     }
 }
