abafef9fbaf81216e6113796ede773c25be84c18
Campbell Strange <campbell.strange@live.com.au>
2026-05-04 12:28:27 +1000

    Fixed UI bug of windows not maximizing or adjusting window when clicking different pages.

--- src/main/java/com/stockle/ui/SceneManager.java
+++ src/main/java/com/stockle/ui/SceneManager.java
@@ -9,6 +9,8 @@ import javafx.stage.Stage;
 public class SceneManager {
 
     private static Stage stage;
+    private static boolean isMaximized = true;
+
 
     public static void init(Stage primaryStage) {
         stage = primaryStage;
@@ -16,10 +18,21 @@ public class SceneManager {
 
     public static void switchTo(String fxmlFile) throws IOException {
         FXMLLoader loader = new FXMLLoader(SceneManager.class.getResource(fxmlFile));
-        Scene scene = new Scene(loader.load());
-        scene.getStylesheets().clear();
+
+        boolean wasMaximized = stage.isMaximized();
+        double width = stage.getWidth();
+        double height = stage.getHeight();
+        
+        Scene scene = new Scene(loader.load(), width, height);
+
         stage.setScene(scene);
-        stage.setMaximized(true);
+
+        if (wasMaximized) {
+            javafx.application.Platform.runLater(() -> stage.setMaximized(true));
+        } else {
+            stage.setWidth(width);
+            stage.setHeight(height);
+        }
     }
 
     public static Stage getStage() {
