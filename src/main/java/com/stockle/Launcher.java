8adeb3e2dc50f5f3ac9f1b816e64b1ba39c84f44
JJTSmasher <jamescannon0514@gmail.com>
2026-06-07 00:36:05 +1000

    comments

--- src/main/java/com/stockle/Launcher.java
+++ src/main/java/com/stockle/Launcher.java
@@ -4,6 +4,9 @@ import com.stockle.ui.StockleApplication;
 
 import javafx.application.Application;
 
+/**
+ * Launcher class to start the JavaFX application
+ */
 public class Launcher {
     public static void main(String[] args) {
         Application.launch(StockleApplication.class, args);
