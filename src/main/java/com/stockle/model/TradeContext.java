cfae5196ca75e03e529e4efbc4ab5ecb5205f80e
Ethan Bell <ethbot1112@gmail.com>
2026-06-06 21:56:05 +1000

    Commented More Files

--- src/main/java/com/stockle/model/TradeContext.java
+++ src/main/java/com/stockle/model/TradeContext.java
@@ -1,11 +1,21 @@
 package com.stockle.model;
 
+/**
+ * The TradeContext class represents the context of a trade, including the action taken, the change in price, the profit or loss from the trade, and the number of days the position was held.
+ */
 public class TradeContext {
     public String action;
     public double priceChangePercent;
     public double profitLoss;
     public int holdingDays;
 
+    /**
+     * Constructs a new TradeContext object with the specified values.
+     * @param action the action taken (buy/sell)
+     * @param priceChangePercent the percentage change in price
+     * @param profitLoss the profit or loss from the trade
+     * @param holdingDays the number of days the position was held
+     */
     public TradeContext(String action, double priceChangePercent, double profitLoss, int holdingDays) {
         this.action = action;
         this.priceChangePercent = priceChangePercent;
